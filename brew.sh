562bb4c757a4fba9cf856fcb6fb41fc6a0b253af
Evangelos <github@evangelos.com>
2022-03-03 14:57:31 -0400

    changing version number of ruby

--- brew.sh
+++ brew.sh
@@ -83,8 +83,8 @@ gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D695
 \curl -sSL https://get.rvm.io | bash -s stable
 source ~/.bash_profile
 # Install Ruby & Rails
-rvm install 2.6.3
-rvm use 2.6.3
+rvm install 2.7.3
+rvm use 2.7.3
 gem install rails
 
 

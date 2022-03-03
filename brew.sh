9d6fc914d69af4c418b40bd83187cee99b76908d
Evangelos <github@evangelos.com>
2022-03-03 13:54:02 -0400

    sourcing bash profile so rvm will work

--- brew.sh
+++ brew.sh
@@ -81,6 +81,7 @@ brew install gnupg
 gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 # Install rvm
 \curl -sSL https://get.rvm.io | bash -s stable
+source ~/.bash_profile
 # Install Ruby & Rails
 rvm install 2.6.3
 rvm use 2.6.3
